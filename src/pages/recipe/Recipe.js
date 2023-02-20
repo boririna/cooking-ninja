026e0ed53957f5064c5d8b5d61fa85c0b061d79d
Irina Borisova <i.a.borisova@gmail.com>
2023-02-20 17:59:54 +0100

    recipe page is ready

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,17 +1,26 @@
 import { useFetch } from '../../hooks/useFetch'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 //styles
 import './Recipe.css'
 
 export default function Recipe() {
   const { id } = useParams()
-  const {data, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
+  const {data: recipe, isPending, error} = useFetch(`http://localhost:3000/recipes/${id}`)
   return (
-    <div>
+    <div className="recipe">
       {error && <p className="error">{error}</p>}
       {isPending && <p className="loading">Loading...</p>}
-      {data && <p>{data.title}</p>}
+      {recipe && (
+        <>
+          <h3 className="page-title">{recipe.title}</h3>
+          <p>Takes {recipe.cookingTime} to cook</p>
+          <ul>
+            {recipe.ingredients.map(ing => <li key={ing}>{ing}</li>)}
+          </ul>
+          <p className="method">{recipe.method}</p>
+        </>
+      )}
     </div>
   )
 }
