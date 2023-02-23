041f51d8212b592391cf0e424d3a8ca1b2d75575
Irina Borisova <i.a.borisova@gmail.com>
2023-02-23 22:18:30 +0100

    added post method to useFetch, made redirection, added search

--- src/pages/create/create.js
+++ src/pages/create/create.js
@@ -1,4 +1,6 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { useHistory } from "react-router-dom"
+import { useFetch } from '../../hooks/useFetch'
 
 //styles
 import './Create.css'
@@ -11,11 +13,13 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('')
   const [ingredients, setIngredients] = useState([])
   const ingredientInput = useRef(null)
+  const history = useHistory()
 
+  const { postData, data, error } = useFetch('http://localhost:3000/recipes', 'POST')
 
-  const handelSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title, method, cookingTime, ingredients)
+    postData({ title, ingredients, method, cookingTime: cookingTime + 'minutes' })
   }
 
   const handleAdd = (e) => {
@@ -28,11 +32,23 @@ export default function Create() {
     ingredientInput.current.focus()
   }
 
+  // Redirect the user when we get data response
+
+  useEffect(() => {
+    const timeout = () => {
+      if (data) {
+        history.push('/')
+      }
+    }
+    setTimeout(timeout, 5000)
+    
+  }, [data])
+
   return (
     <div className="create">
       <h2 className="page-title">Add a New Recipe</h2>
 
-      <form onSubmit={handelSubmit}>
+      <form onSubmit={handleSubmit}>
 
         <label>
           <span>Recipe title:</span>
